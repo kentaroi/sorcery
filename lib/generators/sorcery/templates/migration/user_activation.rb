1f248bc7187111cb9e6912cb8587299d06471b59
Kentaro Imai <kentaroi@gmail.com>
2012-03-12 20:18:44 +0900

    change migration for email verification

--- lib/generators/sorcery/templates/migration/user_activation.rb
+++ lib/generators/sorcery/templates/migration/user_activation.rb
@@ -3,13 +3,15 @@ class SorceryUserActivation < ActiveRecord::Migration
     add_column :<%= model_class_name.tableize %>, :activation_state, :string, :default => nil
     add_column :<%= model_class_name.tableize %>, :activation_token, :string, :default => nil
     add_column :<%= model_class_name.tableize %>, :activation_token_expires_at, :datetime, :default => nil
-    
+    # add_column :<%= model_class_name.tableize %>, :pending_email, :string, :default => nil
+
     add_index :<%= model_class_name.tableize %>, :activation_token
   end
 
   def self.down
     remove_index :<%= model_class_name.tableize %>, :activation_token
-    
+
+    # remove_column :<%= model_class_name.tableize %>, :pending_email
     remove_column :<%= model_class_name.tableize %>, :activation_token_expires_at
     remove_column :<%= model_class_name.tableize %>, :activation_token
     remove_column :<%= model_class_name.tableize %>, :activation_state
