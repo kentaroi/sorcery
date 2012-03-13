1dcda963595ae60a026adeaa1bb4725e71980ed4
Kentaro Imai <kentaroi@gmail.com>
2012-03-13 09:49:54 +0900

    Fix migration file for email verification test

--- spec/rails3/db/migrate/activation/20101224223622_add_activation_to_users.rb
+++ spec/rails3/db/migrate/activation/20101224223622_add_activation_to_users.rb
@@ -3,6 +3,7 @@ class AddActivationToUsers < ActiveRecord::Migration
     add_column :users, :activation_state, :string, :default => nil
     add_column :users, :activation_token, :string, :default => nil
     add_column :users, :activation_token_expires_at, :datetime, :default => nil
+    add_column :users, :pending_email, :string, :default => nil
     
     add_index :users, :activation_token
   end
@@ -10,6 +11,7 @@ class AddActivationToUsers < ActiveRecord::Migration
   def self.down
     remove_index :users, :activation_token
     
+    remove_column :users, :pending_email
     remove_column :users, :activation_token_expires_at
     remove_column :users, :activation_token
     remove_column :users, :activation_state
