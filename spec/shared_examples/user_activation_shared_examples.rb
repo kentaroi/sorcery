c86a5cd2fb5eac3f6d7ddfbf98d85058e502f07e
Kentaro Imai <kentaroi@gmail.com>
2012-03-13 09:33:54 +0900

    Add tests for email verification

--- spec/shared_examples/user_activation_shared_examples.rb
+++ spec/shared_examples/user_activation_shared_examples.rb
@@ -47,6 +47,26 @@ shared_examples_for "rails_3_activation_model" do
     it "if mailer is disabled and mailer is nil, do NOT throw exception" do
       expect{sorcery_reload!([:user_activation], :activation_mailer_disabled => true)}.to_not raise_error
     end
+
+    it "should enable configuration option 'email_verification_on_change_disabled'" do
+      sorcery_model_property_set(:email_verification_on_change_disabled, :my_email_verification_disabled)
+      User.sorcery_config.email_verification_on_change_disabled.should equal(:my_email_verification_disabled)
+    end
+
+    it "should enable configuration option 'pending_email_attribute_name'" do
+      sorcery_model_property_set(:pending_email_attribute_name, :my_pending_email)
+      User.sorcery_config.email_verification_on_change_disabled.should equal(:my_pending_email)
+    end
+
+    it "should enable configuration option 'email_verification_needed_email_method_name'" do
+      sorcery_model_property_set(:email_verification_needed_email_method_name, :my_verification_email)
+      User.sorcery_config.email_verification_needed_email_method_name.should equal(:my_verification_email)
+    end
+
+    it "should enable configuration option 'email_verification_success_email_method_name'" do
+      sorcery_model_property_set(:email_verification_success_email_method_name, :my_verification_email)
+      User.sorcery_config.email_verification_success_email_method_name.should equal(:my_verification_email)
+    end
   end
 
   # ----------------- ACTIVATION PROCESS -----------------------
@@ -198,6 +218,156 @@ shared_examples_for "rails_3_activation_model" do
       sorcery_model_property_set(:activation_token_expiration_period, nil)
       create_new_user
       User.load_from_activation_token(@user.activation_token).should == @user
+    end
+  end
+
+  # ----------------- EMAIL VERIFICATION PROCESS ---------------
+  describe User, "email verification process" do
+    context "email verification on change has been enabled" do
+      before(:all) do
+        sorcery_reload!([:user_activation], :email_verification_on_change_disabled => false, :user_activation_mailer => ::SorceryMailer)
+      end
+      before(:each) do
+        create_new_user
+      end
+
+      specify { @user.should respond_to(:verify_email!) }
+
+      context "when user has not been activated" do
+        context "when set new email address" do
+          before do
+            @old_email = @user.email
+            @new_email = 'new@example.com'
+            @user.email = @new_email
+          end
+        end
+
+        it "should behave normal setter method" do
+          @user.email.should == @new_email
+          @user.pending_email.should be_nil
+          @user.activation_token.should be_nil
+        end
+
+        context "when User#save" do
+          before do
+            @old_size = ActionMailer::Base.deliveries.size
+            @user.save
+          end
+
+          it "should not swap back email addresses and hold new email address in email attribute and should not send the user a verification email" do
+            @user.email.should == @new_email
+            @user.pending_email.should be_nil
+            @user.activation_token.should be_nil
+            ActionMailer::Base.deliveries.size.should == @old_size
+          end
+
+          context "when call verify_email!" do
+            before do
+              @email = @user.email
+              @pending_email = @user.pending_email
+              @activation_token = @user.activation_token
+              @old_size = ActionMailer::Base.deliveries.size
+              @user.verify_email!
+            end
+
+            it "should not do anything" do
+              @user.email.should == @email
+              @user.pending_email.should == @pending_email
+              @user.activation_token.should == @activation_token
+              ActionMailer::Base deliveries.size.should == @old_size
+            end
+          end
+        end
+      end
+
+      context "when user has been activated" do
+        before(:each) do
+          @user.activation_state = 'active'
+          @user.save
+        end
+
+        context "when set new email address" do
+          before do
+            @old_email = @user.email
+            @new_email = 'new@example.com'
+            @user.email = @new_email
+          end
+
+          it "should copy current email address to pending_email attribute and hold new email address in email attribute until User#save" do
+            @user.email.should == @new_email
+            @user.pending_email.should == @old_email
+          end
+
+          context "when User#save" do
+            before do
+              @old_size = ActionMailer::Base.deliveries.size
+              @user.save
+            end
+
+            it "should swap back email addresses and hold new email address in pending_email attribute and should send the user a verification email" do
+              @user.email.should == @old_email
+              @user.pending_email.should == @new_email
+              @user.activation_token.should_not be_nil
+              ActionMailer::Base.deliveries.size.should == @old_size + 1
+            end
+
+            context "when verified email successfully" do
+              before do
+                @old_size = ActionMailer::Base.deliveries.size
+                @user.verify_email!
+              end
+
+              it "should have new email address, clear pending_email and email verification code and send the user a verification success email" do
+                @user2 = User.find(@user.id) # go to db to make sure it was saved and not just in memory
+                @user2.email.should == @new_email
+                @user2.pending_email.should be_nil
+                @user2.activation_token.should be_nil
+                ActionMailer::Base deliveries.size.should == @old_size + 1
+              end
+            end
+          end
+        end
+      end
+    end
+
+    context "email verification on change has been disabled" do
+      before(:all) do
+        sorcery_reload!([:user_activation], :email_verification_on_change_disabled => true, :user_activation_mailer => ::SorceryMailer)
+      end
+      before(:each) do
+        create_new_user
+        @user.activation_state = 'active'
+        @user.save
+      end
+
+
+      context "when set new email address" do
+        before do
+          @old_email = @user.email
+          @new_email = 'new@example.com'
+          @user.email = @new_email
+        end
+
+        it "should behave normal setter method" do
+          @user.email.should == @new_email
+          @user.pending_email.should be_nil
+          @user.activation_token.should be_nil
+        end
+
+        context "when User#save" do
+          before do
+            @old_size = ActionMailer::Base.deliveries.size
+            @user.save
+          end
+
+          it "should not swap back email addresses and hold new email address in email attribute and should not send the user a verification email" do
+            @user.email.should == @new_email
+            @user.pending_email.should be_nil
+            @user.activation_token.should be_nil
+            ActionMailer::Base.deliveries.size.should == @old_size
+          end
+        end
+      end
     end
   end
 end
